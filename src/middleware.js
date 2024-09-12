793406470cd54880e4813a9d63b385a146ac60bb
Xayida83 <lotta.lindberg83@gmail.com>
2024-09-12 22:36:02 +0200

    ...

--- src/middleware.js
+++ src/middleware.js
@@ -5,43 +5,6 @@ const unsafeMethods = ["POST", "PUT", "DELETE"];
 
 //* middleware loggar varje förfrågning och hämtar URL för att analysera förfrågningsvägen.
 //* Hämtar den aktuella URL-vägen för att kunna kontrollera om det är en väg som kräver autentisering.
-// export async function middleware(req) {
-  
-//   const url = new URL(req.url);
-
-//   //* Middleware-funktionen kontrollerar om förfrågan använder en av de "osäkra" metoderna (POST, PUT, eller DELETE), eller om vägen innehåller /api/users. Om så är fallet, initierar den en JWT-verifieringsprocess.
-//   if (
-//     unsafeMethods.includes(req.method) ||
-//     url.pathname.includes("api/users")
-//   ) {
-//     console.log("VERIFY");
-
-//     try {
-//       const token = getAuthHeader(req);
-//        // Använt getAuthHeader för att hämta token
-//       if (!token) {
-//         throw new Error("No token submitted");
-//       }
-
-//       const jwtPayload = await verifyJWT(token);
-//       console.log("Decoded JWT: ", jwtPayload); // Logga dekodad JWT
-
-//       //* Om token verifieras, sätts användarens ID (från jwtPayload.userId) i förfrågningens headers.
-//       const headers = new Headers(req.headers);
-//       headers.set("userId", JSON.stringify(jwtPayload.userId));
-
-//       //* Fortsätter med förfrågningen och skickar med de uppdaterade headers.
-//       return NextResponse.next({ headers: headers });
-//     } catch (error) {
-//       return NextResponse.json(
-//         {
-//           error: "Unauthorized request",
-//         },
-//         { status: 401 }
-//       );
-//     }
-//   }
-// }
 
 export async function middleware(req) {
   // Använd getAuthHeader för att extrahera token från request
